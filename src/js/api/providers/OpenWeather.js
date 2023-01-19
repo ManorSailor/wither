62f046626da830872c9e9157f7d19088b4001c23
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-01-19 12:10:28 +0530

    feat: Implement OpenWeather weather provider

---
+++ src/js/api/providers/OpenWeather.js
@@ -0,0 +1,57 @@
+// API specific implementation with an adapter method to adapt data according to our app's need
+// Singleton + Adapter pattern
+import { fetchData, formatDate } from '../../utils';
+
+const OpenWeather = (() => {
+  const KEY = '49257f6591cfc3ed8daf0b5970d519cb';
+
+  const fetchWeather = ({ lat, lon }) =>
+    fetchData(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${KEY}&units=metric&exclude=hourly,minutely,alerts`
+    );
+
+  // Adapt API data into a format our app expects
+  const formatWeather = ({
+    current: { temp, humidity, visibility, weather, feels_like, wind_speed },
+    daily,
+  }) => {
+    const current = {
+      temp,
+      humidity,
+      visibility,
+      dateTime: formatDate(new Date()),
+      feelsLike: feels_like,
+      windSpeed: wind_speed,
+      weather: weather[0]?.description,
+      weatherGroup: weather[0]?.main,
+      weatherIcon: weather[0]?.icon,
+      weatherID: weather[0]?.id,
+    };
+
+    const weekly = daily?.reduce(
+      (next7Days, { temp, weather, dt, wind_speed }) => {
+        const formattedDay = {
+          date: formatDate(dt, 'MMM do'),
+          tempMin: temp.min,
+          tempMax: temp.max,
+          windSpeed: wind_speed,
+          weather: weather[0]?.description,
+          weatherGroup: weather[0]?.main,
+          weatherIcon: weather[0]?.icon,
+          weatherID: weather[0]?.id,
+        };
+
+        next7Days.push(formattedDay);
+
+        return next7Days;
+      },
+      []
+    );
+
+    return { current, weekly };
+  };
+
+  return { fetchWeather, formatWeather };
+})();
+
+export default OpenWeather;
