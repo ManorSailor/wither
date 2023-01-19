a86137c941706b6f7165dc4d3c94843e0c40be1e
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-01-19 12:11:11 +0530

    feat: Implement OpenWeather GeoCodeAPI location provider

---
+++ src/js/api/providers/GeoCodeAPI.js
@@ -0,0 +1,26 @@
+// API specific implementation with an adapter method to adapt data according to our app's need
+// Singleton + Adapter pattern
+import { fetchData } from '../../utils';
+
+const GeoCodeAPI = (() => {
+  const LIMIT = 8;
+  const KEY = 'bcb623dd560f25e7471cc8e69b50ce19';
+
+  const fetchLocations = (location) =>
+    fetchData(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${LIMIT}&appid=${KEY}`
+    );
+
+  const formatLocation = ({ name, country, lat, lon }) => {
+    return {
+      lat,
+      lon,
+      country,
+      city: name,
+    };
+  };
+
+  return { fetchLocations, formatLocation };
+})();
+
+export default GeoCodeAPI;
