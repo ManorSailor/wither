78352e03390fbb177a534a5d96aab365b9af8bcf
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-01-20 19:44:50 +0530

    feat: MVP weather app console variant

--- src/app.js
+++ src/app.js
@@ -1,2 +1,23 @@
 import './css/styles.css';
 import 'iconify-icon';
+import LocationAPI from './js/api/LocationAPI';
+import WeatherAPI from './js/api/WeatherAPI';
+
+async function getUserQuery(query) {
+  if (isValidQuery(query)) {
+    try {
+      const location = await LocationAPI.getLocation(query);
+      const weather = await WeatherAPI.getWeather(location);
+      console.log(weather);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+function isValidQuery(query) {
+  return query && query.trim() !== '';
+}
+
+// Expose the function to the user for finding the weather
+window.search = getUserQuery;
